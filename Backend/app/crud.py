793ee7a739a2d1932af5ed5ff77cefffc3828147
Miocoji <your_email@example.com>
2025-07-17 13:57:12 +0900

    chore: crud.pyからFirebase管理APIの動作確認用修正

--- Backend/app/crud.py
+++ Backend/app/crud.py
@@ -14,11 +14,43 @@ def create_user(db: Session, user: schemas.UserCreate):
     return db_user
 
 # --- DailyReportのCRUD処理 ---
-# TO DO:中身を実装してください（担当：日報一覧担当者）
-def get_daily_reports(db: Session, owner_id: str, skip: int = 0, limit: int = 100):
-    # TO DO: ここに日報一覧を取得するロジックを実装してください
-    return [] # とりあえず空のリストを返すようにしています
+# note :日報一覧情報(降順)を取得するCRUD（担当：日報一覧担当者）
+def get_daily_reports(db: Session, owner_id: str, skip: int = 0, limit: int = 10):
+    return db.query(models.DailyReport).filter(
+        models.DailyReport.owner_id == owner_id
+        ).order_by(
+            models.DailyReport.report_date.desc()
+        ).offset(skip).limit(limit).all()
 
+# note :特定の一つの日報を取得するCRUD
+def get_daily_report(db: Session, report_id: int, owner_id: str):
+    return db.query(models.DailyReport).filter(
+        models.DailyReport.id == report_id,
+        models.DailyReport.owner_id == owner_id
+    ).first()
+
+# note :特定のユーザーの日報を作成するCRUD（担当：日報新規作成担当者）
 def create_daily_report(db: Session, report: schemas.DailyReportCreate, owner_id: str):
-    # TO DO: ここに日報を作成するロジックを実装してください（担当：日報新規作成・編集担当者）
-    pass
+    db_report = models.DailyReport(**report.model_dump(), owner_id=owner_id)
+    db.add(db_report)
+    db.commit()
+    db.refresh(db_report)
+
+# note :特定のユーザーの日報を取編集するCRUD（担当：日報編集担当者） 
+def edit_daily_report(db: Session, report_id: int, report_update: schemas.DailyReportCreate, owner_id: str):
+    db_report = db.query(models.DailyReport).filter(
+        models.DailyReport.id == report_id,
+        models.DailyReport.owner_id == owner_id
+    ).first()
+
+    if db_report is None:
+        return None
+    
+    update_data = report_update.model_dump(exclude_unset=True)
+
+    for key, value in update_data.items():
+        setattr(db_report, key, value)
+
+    db.commit()
+    db.refresh(db_report)
+    return db_report    
