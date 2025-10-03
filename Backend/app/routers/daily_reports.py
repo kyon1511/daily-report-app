1161b8ad129eb37b6d43bfac7102367f5621390b
Kyoko Kumagai <kyon@KyokonoMacBook-Air.local>
2025-10-04 02:30:25 +0900

    feat:daily_reports.py に単体取得用エンドポイントを追加

--- Backend/app/routers/daily_reports.py
+++ Backend/app/routers/daily_reports.py
@@ -31,6 +31,19 @@ def read_reports(
     """
     return crud.get_daily_reports(db, owner_id=current_user.id)
 
+@router.get("/{report_id}", response_model=schemas.DailyReport)
+def read_report(
+    report_id: int,
+    db: Session = Depends(get_db),
+    current_user: models.User = Depends(auth.get_current_user),
+):
+    """
+    ログインしているユーザーが指定した日報を1件取得するためのAPIエンドポイント
+    """
+    db_report = crud.get_daily_report(db, report_id=report_id, owner_id=current_user.id)
+    if db_report is None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="日報が見つかりません")
+    return db_report
 
 @router.put("/{report_id}", response_model=schemas.DailyReport)
 def update_report(
